5a817e409e11edeecf686e2dd5fc48dd6c9163c0
Steff-ee <dsteffee@alumni.stanford.edu>
2022-06-29 21:40:18 -0700

    Fixing CloudedIce color

--- src/shared/helpers/constants.ts
+++ src/shared/helpers/constants.ts
@@ -6,7 +6,7 @@ export enum Colors {
 	DarkBlue = '#1C304A',
 	IceBlue = '#e1f8ff',
 	IceWhite = '#f0f9ff',
-	CloudedIce = '#f0f9ff',
+	CloudedIce = '#daebf7',
 	SeaFoam = '#6ec9d4',
 	WarmSand = '#d4ad6e',
 	LightSand = '#fffae8',
