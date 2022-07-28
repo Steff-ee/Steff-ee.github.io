743676a7e88ff9e47f466bbfb549cf11ae5d2860
Steff-ee <dsteffee@alumni.stanford.edu>
2022-07-28 09:43:07 -0700

    Post 134 edits

--- src/shared/posts/store/POST_00134.tsx
+++ src/shared/posts/store/POST_00134.tsx
@@ -17,13 +17,13 @@ const PostContent: React.FunctionComponent = (): JSX.Element => {
 				<b>Designation:</b> PCS-EEAAO
 			</p>
 			<p>
-				<b>Status:</b> Extent and active.
+				<b>Status:</b> Extent/active.
 			</p>
 			<p>
 				<b>Tags:</b> Nonsentient. Reproducible. Visible/mundane.
 			</p>
 			<p>
-				<b>Discovered:</b> March 25, 2022, Calendar Prime.
+				<b>Discovered:</b> March 25, 2022, calendar Standard/prime.
 			</p>
 
 			<p>
@@ -37,21 +37,23 @@ const PostContent: React.FunctionComponent = (): JSX.Element => {
 			</p>
 
 			<p>
-				<b>Description:</b> When inactive, an instance of PCS-EEAAO is alternatingly an
-				earpiece, a wristwatch, or a fake mustache. When worn, it can be activated by
-				[REDACTED]. When activated, the subject will have their mind swapped with a
-				multiversal other-self. [See PCS-EE001 for information regarding the multiverse; see
-				PCS-AC006 for information regarding the continuity of mind and actions that subvert
-				it.] The other-self will control the subject-prime’s body while subject-prime
-				controls other-self’s body. In testing, this control swap lasts up to 24 hours [See
-				Addendum 5], though no upper limit to the duration of swapping has yet been
-				theorized [See Addendum 5b].
+				<b>Description:</b> When inactive, an instance of PCS-EEAAO alternatingly manifests
+				as either an earpiece, a wristwatch, or a fake mustache. When worn, it can be
+				activated by [REDACTED]. When activated, the wearer (hereafter referred to as the
+				"subject" or "subject-prime") will have their mind swapped with a multiversal
+				other-self. [See PCS-EE001 for information regarding the multiverse; see PCS-AC006
+				for information regarding the continuity of mind and actions that subvert it.] The
+				other-self will control subject-prime’s body while subject-prime controls the
+				other-self’s body. In testing, this control swap lasts up to 24 hours [See Addendum
+				5], though no upper limit to the duration of swapping has yet been theorized [See
+				Addendum 5b].
 			</p>
 			<p>
-				The steps outlined above seem to be necessary but not alone sufficient for
-				activation of PCS-EEAAO. A significant synchronicity between the minds of prime-self
-				and other-self is highly likely a requirement. Dr. Quan says that both selves must
-				be “one and the same” for activation to occur. [See Addendum 1 for data points.]
+				The activation steps outlined above seem to be necessary but not alone sufficient to
+				trigger a multiverse mind swap with PCS-EEAAO. A significant synchronicity between
+				the minds of prime-self and other-self is highly likely a requirement. According to
+				researcher Dr. Quan, both selves must be “one and the same” for activation to occur.
+				[See Addendum 1 for data points.]
 			</p>
 
 			<Divider />
@@ -65,76 +67,87 @@ const PostContent: React.FunctionComponent = (): JSX.Element => {
 			</p>
 
 			<p>
-				<b>Activation 1:</b> Verified, Foundational. Dr. Quan was an assistant in the
-				initial investigations into PCS-EEAAO and also a voluntary subject in test trials.
-				After activating PCS-EEAAO, Dr. Quan swapped with an other-self. At the time of
-				swapping, Dr. Quan’s other-self was investigating their other-version of PCS-EEAAO
-				in an identical manner to the investigation-prime. The swap lasted for 23 hours and
-				58 minutes, during which neither version of Dr. Quan was able to discern any
+				<b>Activation 1:</b> Verified/Foundational. Dr. Quan was an assistant in the initial
+				investigations into PCS-EEAAO and also a voluntary subject in test trials. After
+				activating PCS-EEAAO, Dr. Quan swapped with an other-self. At the time of swapping,
+				Dr. Quan’s other-self was investigating their other-version of PCS-EEAAO in an
+				identical manner to investigation-prime. The swap lasted for 23 hours and 58
+				minutes, during which neither version of Dr. Quan was able to discern any
 				differences between verse-prime and other-verse. [See Addendum 1b for test notes.]
 			</p>
 
 			<p>
-				<b>Activation 2:</b> Verified, Foundational. In a follow-up to Activation 1, under
+				<b>Activation 2:</b> Verified/Foundational. In a follow-up to Activation 1, under
 				similar circumstances and variables controlled, Dr. Quan underwent another swap,
-				this time while playing a solitary card game involving a great deal of chance. After
-				swapping, minor differences between the verses became immediately evident. No major
-				differences were discovered. [See Addendum 1b for test notes.]
+				this time while playing a solitary card game involving chance. After swapping, minor
+				differences between the verses became immediately evident. No major differences were
+				discovered. [See Addendum 1c for test notes.]
 			</p>
 
 			<p>
-				<b>Activation 3:</b> Verified, external. Subject [REDACTED] swapped with their
+				<b>Activation 3:</b> Verified/external. Subject [REDACTED] swapped with their
 				other-self despite both selves being located in different positions on Earth,
 				[REDACTED]. They were asleep at the time of swapping and reported having dreamed the
-				same dream. Swap lasted 1 hour.
+				same dream. Swap lasted 1 hour. Other-Earth was reported to have similar yet
+				distinct geopolitics. Subject speculated that the other-verse branched on June 7,
+				2016, calendar Standard. [See Addendum 1c for subject interview.]
 			</p>
 
 			<p>
-				<b>Activations 4-11:</b> Unverified, plausible. Following [Incident EE-222], all of
+				<b>Activations 4-11:</b> Unverified/plausible. Following [Incident EE-222], all of
 				the following subjects experienced swaps: [REDACTED]. One activation involved
 				inebriants, another involved meditation, and the rest involved moments of highly
 				focused “flow”, i.e., the mental state exhibited when subjects engaged in the
 				playing of music, sports, or in one case, fornication. Swaps lasted between 16 to 21
-				hours.
+				hours. Other-verses were reportedly distinct to significant degrees, but all
+				accounts are unreliable. [See Incident EE-222 for more information.]
 			</p>
 
 			<p>
-				<b>Activations 12-47:</b> Verified, external. During [Incident EE-223], fifty
-				members of a cult all attempted activation during a ritual involving inebriants and
+				<b>Activations 12-47:</b> Verified/external. During [Incident EE-223], fifty members
+				of a cult all attempted activation during a ritual involving inebriants and
 				practices intended to induce meditative flow states. Thirty-six subjects succeeded.
 				The subjects who succeeded skewed younger. The [REDACTED] older subjects who
 				succeeded had shorter swaps, lasting on average 5 hours, while the longest swaps
 				were had by the [REDACTED] children, each lasting 24 hours. It is theorized that
 				less complex minds can more easily synchronize. Dr. Quan: “Age and experience bring
 				with them growing differences, branching etchings on our multiversal blank slates.”
+				[See Incident EE-223 for more information.]
 			</p>
 
 			<p>
-				<b>Activation 48:</b> Unverified, impossible.
+				<b>Activation 48:</b> Unverified/impossible.
 			</p>
 
 			<p>
-				<b>Activation 49:</b> Debated, plausible. During [Incident EE-318], child celebrity
-				[REDACTED] swapped into their twin sibling’s other-self. Swap lasted 12 hours.
+				<b>Activation 49:</b> Debated/plausible. During [Incident EE-318], child celebrity
+				[REDACTED] swapped into their twin sibling’s other-self. Swap lasted 12 hours. The
+				other-verse was reportedly identical to verse-prime in geopolitics from a macro
+				scale, but differed innumerably from a micro scale. [See Incident EE-318 for more
+				information.]
 			</p>
 
 			<p>
-				<b>Activation 50:</b> Verified, external. Dr. Quan, no longer with the Foundation
+				<b>Activation 50:</b> Verified/external. Dr. Quan, no longer with the Foundation
 				after [Incident EE-226], became the lead guitarist of a rock cover band. The band
-				performed at a British musical festival, giving the biggest performance of their
-				careers, for an audience of approximately 300 rock fans, mostly British citizens.
-				Inebriants were had prior to the performance. During the encore, Dr. Quan engaged in
-				a particularly intricate guitar solo and activated PCS-EEAAO. Despite the
-				differences in their identity, inclinations, intelligence, and gender, Dr. Quan
-				swapped into the famed guitarist [REDACTED]. Swap lasted 12 minutes, 53 seconds.
+				performed at a British musical festival, giving the biggest performance of the
+				band's history, for an audience of approximately 300 British rock fans. Inebriants
+				were had prior to the performance. During the encore, Dr. Quan engaged in a
+				particularly intricate guitar solo and activated PCS-EEAAO. Despite the differences
+				in their identity, inclinations, intelligence, and gender, Dr. Quan swapped into the
+				multiversal mind of famed guitarist [REDACTED]. Swap lasted 12 minutes, 53 seconds.
+				No differences between verses discerned. [See Incident EE-226 and EE-227 for more
+				information.]
 			</p>
 
 			<p>
-				<b>Activation 51:</b> Verified, external. Subjects were a married couple, ages 90
-				and 91, both now deceased. They were together engaged in a peaceful, loving
-				afternoon of contemplative leisure. Witnesses report that the couple knew they would
-				soon pass. They activated two instances of PCS-EEAAO and passed shortly after the
-				swap’s ending. Unknown swap duration.
+				<b>Activation 51:</b> Verified/external. Subjects were a married couple, ages 90 and
+				91, both now deceased. They were together engaged in a peaceful, loving afternoon of
+				contemplative leisure. Witnesses reported that the couple knew they would soon pass.
+				They activated two instances of PCS-EEAAO. Shortly after the swap’s conclusion, the
+				couple passed together in their sleep. Unknown swap duration. Unknown if differences
+				between verses were discerned. [Documentation MISSING. Does not appear to be an
+				official redaction nor a system error. Will investigate. - Ed.]
 			</p>
 		</>
 	)
@@ -149,6 +162,7 @@ export const POST_00134: IPost = {
 			<a href="https://scp-wiki.wikidot.com/" target="_blank">
 				SCP
 			</a>
+			.
 		</>
 	),
 	createdTime: 1658990000000,
